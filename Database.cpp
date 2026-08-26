8ca198194f4576e7d28804ecb6688430e66a2b2c
Evan Graves <evanb@MacBook-Air.local>
2026-08-26 11:52:37 -0500

    Add username and password database project files

---
+++ Database.cpp
@@ -0,0 +1,3 @@
+// need an array for the passwords and usernames
+char username[4][50];
+char password[4][20];
---
+++ usercreation.h
@@ -0,0 +1,9 @@
+#ifndef USERCREATION_H
+#define USERCREATION_H
+
+#include "Database.h"
+
+void createUser(Database& database);
+void loginUser(const Database& database);
+
+#endif
